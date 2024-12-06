85e41e701011fe1c1dcc5b39bdec052f52c80e21
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2024-12-06 09:35:45 -0600

    new bundle

--- android/app/src/main/java/com/mangoticket/mangoticketapp/MainActivity.kt
+++ android/app/src/main/java/com/mangoticket/mangoticketapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.rodrwan.mangoticket
+package com.mangoticket.mangoticketapp
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/mangoticket/mangoticketapp/MainApplication.kt
+++ android/app/src/main/java/com/mangoticket/mangoticketapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.rodrwan.mangoticket
+package com.mangoticket.mangoticketapp
 
 import android.app.Application
 import android.content.res.Configuration
